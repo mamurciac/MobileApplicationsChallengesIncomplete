7cee21a32d8aec190774bf1b63b5822d41658e39
Michael Andrés <mamurciac@unal.edu.co>
2019-04-06 15:09:59 -0500

    App Init

---
+++ Challenge5_TicTacToe/app/src/main/java/mamurciac/unal/edu/co/challenge5_tictactoe/MainActivity.java
@@ -0,0 +1,13 @@
+package mamurciac.unal.edu.co.challenge5_tictactoe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
